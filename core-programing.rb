7a6a0d8766d56fed2aeb305a77180b80d23bb37b
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-10 14:21:15 -0500

    problem 4

--- core-programing.rb
+++ core-programing.rb
@@ -31,7 +31,27 @@
 
 # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
 
+# x = 9002
+
+# if x > 9000
+#   p 1
+# else
+#   p -1
+# end
+
 # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+# x = 88
+
+# if x < 10
+#   p 9
+# elsif x < 20
+#   p 19
+# elsif x < 30
+#   p 29
+# else 
+#   p -1
+# end
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
