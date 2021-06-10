616afa59f0b9728a07572451cde086419a83eecb
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-10 14:23:14 -0500

    problem 5

--- core-programing.rb
+++ core-programing.rb
@@ -55,6 +55,8 @@
 
 # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
 
+
+
 # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
 # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
