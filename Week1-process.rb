261e3775c5383017848eabcb728b590f36901686
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-12 17:33:10 -0500

    finish week 1 process

--- Week1-process.rb
+++ Week1-process.rb
@@ -15,23 +15,52 @@
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-p "Enter a word:"
-answer = gets.chomp
-if answer == "marco"
-  p "polo"
-end
+# p "Enter a word:"
+# answer = gets.chomp
+# if answer == "marco"
+#   p "polo"
+# end
 
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
 
+# x = "red"
+# y = "blue"
+# z = "green"
+
+# p "My three favorite colors are " + x + ", " + y + " and " + z
+
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+# x = "red"
+# y = "blue"
+# z = "green"
+
+# p "My three favorite colors are #{x}, #{y}, and #{z}."
 
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
 
+# p "Enter your name:"
+# name = gets.chomp
+
+# if name.downcase != "santa"
+#   p "You're not Santa"
+# end
+
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
 
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+# p "Enter a password:"
+# password = gets.chomp
+
+# if password == "Joshua"
+#   p "Shall we play a game"
+# else
+#   p "Access denied"
+# end
 
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
 
