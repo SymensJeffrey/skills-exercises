37b21ee9faf4cfd3bac61dbb3f092903b84c5d82
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-14 13:45:57 -0500

    finish week2 outcome

--- Week2-outcome.rb
+++ Week2-outcome.rb
@@ -37,9 +37,22 @@
 
 # 5. Write a program that asks the user to enter a number, then prints the number times 10.
 
+# p "Enter a number:"
+
+# number_1 = gets.chomp
+
+# 10.times do
+#   p number_1
+# end
+
 # 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
 
 # 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+# p "Enter a word:"
+# answer = gets.chomp
+
+# p answer.length
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
 
@@ -47,5 +60,11 @@
 
 # 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
 
+p "Enter a word:"
+answer = gets.chomp
+
+if answer.length > 5
+  p "Thats a long word"
+end
 
 # SOLUTIONS: https://gist.github.com/peterxjang/1539a3ad79728ba4fb68dd8d07279c29
