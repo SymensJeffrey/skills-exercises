21cf103c7e54e578e6b02cb08c849facf2e56ec8
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-06-14 15:56:38 -0500

    finish week 2 process

--- Week2-process.rb
+++ Week2-process.rb
@@ -24,9 +24,22 @@
 
 # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
 
+# x = 0
+# while x < 101
+#   p x
+#   x += 5
+# end
+
 # 5. Write a while loop that prints the number 9000 ten times.
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+# p "Enter a number"
+#   number = gets.chomp
+# while number.to_i < 11 
+#   p "Enter a number"
+#   number = gets.chomp
+# end
 
 # 7. Write a while loop that prints the numbers 50 to 70.
 
@@ -34,7 +47,20 @@
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
+# p "Enter a word"
+#   word = gets.chomp
+# while word.length < 6
+#   p "Enter a word"
+#   word = gets.chomp
+# end
+
 # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+# x = 2
+# while x < 41
+#   p x
+#   x += 2
+# end
 
 
 # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
